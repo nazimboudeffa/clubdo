c79d0afc4248d5be733872a75f8267550a9f10cc
Nazim Boudeffa <boudeffa@gmail.com>
2025-01-30 22:13:41 +0100

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl">Clique sur le bouton pour écouter le son</h1>
+      <h1 className="text-4xl">Clique sur le bouton pour écouter la chanson de ton anniversaire</h1>
       <button className="text-center rounded-md bg-blue-500 text-white px-4 py-2 mt-4"
         onClick={playAudio}
       >
