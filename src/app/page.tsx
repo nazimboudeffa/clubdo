1055a02fd4d86e731135fb0b89ff8297dd341a95
Nazim Boudeffa <boudeffa@gmail.com>
2025-01-31 00:15:28 +0100

    little fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,15 @@ export default function Home() {
   return (
     <div className="bg-sky-500 min-h-screen flex flex-col items-center justify-center px-16">
       <div className="relative w-full max-w-lg">
-        <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-        <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
-        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+        <div className="absolute top-0 -left-4 w-72 h-72 bg-sky-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+        <div className="absolute top-0 -right-4 w-72 h-72 bg-sky-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div className="absolute -bottom-8 left-20 w-72 h-72 bg-sky-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
       </div>
       <div className="flex flex-col items-center">
         <div className="text-center">
           <Image src={logo} alt="logo" width={200} height={200} />
         </div>
-        <h1 className="text-4xl">Joyeux anniversaire !</h1>
+        <h1 className="text-4xl text-yellow-200">Joyeux anniversaire !</h1>
         <button className="text-center rounded-md bg-blue-500 text-white px-4 py-2 mt-4"
           onClick={playAudio}
         >
@@ -32,7 +32,7 @@ export default function Home() {
         </button>
         <ul className="text-center mt-4">
           {names.map((name, index) => (
-            <li key={index} className="text-xl text-gray-700">{name}</li>
+            <li key={index} className="text-xl text-white">{name}</li>
           ))}
         </ul>
       </div>
