4b026d2631ee3defc5e88488a509c8eb290cfed0
Nazim Boudeffa <boudeffa@gmail.com>
2025-01-30 21:02:12 +0100

    first commit

---
+++ src/lib/liste-name.ts
@@ -0,0 +1,12 @@
+export const names = [
+    "Lucas Morel",
+    "Emma Lefebvre",
+    "Nathan Dubois",
+    "Clara Fontaine",
+    "Hugo Lambert",
+    "Léa Marchand",
+    "Mathis Garnier",
+    "Camille Rousseau",
+    "Noah Perrin",
+    "Sophie Bernard",
+]
